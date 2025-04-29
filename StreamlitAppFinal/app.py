1aa479cf5af0b99169d37982b3674a313c13c9db
Ava M. Dunphy <ava.m.dunphy@gmail.com>
2025-04-29 13:22:55 -0400

    Jam

--- StreamlitAppFinal/app.py
+++ StreamlitAppFinal/app.py
@@ -5,9 +5,12 @@ from spacy import displacy
 
 nlp = spacy.load("en_core_web_sm")
 
+## Import Spotify Tracks Dataset 
 import kagglehub
 
 # Download latest version
 path = kagglehub.dataset_download("maharshipandya/-spotify-tracks-dataset")
 
 print("Path to dataset files:", path)
+
+st.title(" What's Your Jam? 🎧")
