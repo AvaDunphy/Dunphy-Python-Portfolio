85455ad92d455762cb77e14b1da16773e3970137
Ava M. Dunphy <ava.m.dunphy@gmail.com>
2025-04-29 14:17:50 -0400

    Jams

--- StreamlitAppFinal/app.py
+++ StreamlitAppFinal/app.py
@@ -19,27 +19,36 @@ st.markdown("""
 
 BLAH BLAH BLAH BLAH 
 
-## **Follow These Steps to Get Started:**
-1. 
+## Follow These Steps to Get Started:
+1. Look at the data! Take a look at the different kind of music offered, and how it relates to different generes.
+2. Take your quiz! 
+3. In put your mood into music recommendations and find what songs are best for you now. 
 
 """)
 
 
 # Create tabs
-tab1, tab2, tab3 = st.tabs(["Take Mood Quiz", "Music Recommendations", "About This App"])
+tab1, tab2, tab3, tab4 = st.tabs(["Song Data", "Take Mood Quiz", "Music Recommendations", "About This App"])
 
-# Tab 1 - Quiz
+# Tab 1 - Data
 with tab1:
-    st.header("📝 Mood Quiz")
-    st.write("This is where your quiz questions go!")
+    st.header("Song Data")
+    st.write("DATA")
 
-# Tab 2 - Recommendations
+# Tab 2 - Quiz
 with tab2:
-    st.header("🎵 Music Recommendations")
-    st.write("After you find your mood, your music suggestions will show up here!")
+    st.header("📝 Mood Quiz")
+    st.write("Quiz")
 
-# Tab 3 - About
+# Tab 3 - Recomendations
 with tab3:
-    st.header("ℹ️ About")
+    st.header("🎵 Music Recommendation ")
+    st.write("Recs")
+
+# Tab 4 - About
+with tab3:
+    st.header(" ℹ️ About")
     st.write("This app was built to help you discover new music based on your mood. Enjoy!")
+
+   
 
