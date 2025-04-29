7a30f91f544098ccc30492c89b297f7252658333
Ava M. Dunphy <ava.m.dunphy@gmail.com>
2025-04-29 14:01:15 -0400

    Jam

--- StreamlitAppFinal/app.py
+++ StreamlitAppFinal/app.py
@@ -3,6 +3,7 @@ import spacy
 from spacy.pipeline import EntityRuler
 from spacy import displacy
 
+
 nlp = spacy.load("en_core_web_sm")
 
 ## Import Spotify Tracks Dataset 
@@ -11,4 +12,9 @@ import kagglehub
 # Download latest version
 path = kagglehub.dataset_download("maharshipandya/-spotify-tracks-dataset")
 
-st.title("Whats Your Jam?")
+st.title("Whats Your Jam? 🎧 ")
+
+st.markdown(""
+            Hey There! Do you struggle to find the perfect song to fit the mood? Wether you trying to study, cook, 
+            chill out, or get hype. Well look no further! blah blah blah blah blah
+            "")
