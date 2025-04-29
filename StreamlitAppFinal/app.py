4798cc22e7c1e7d0a6a5096c3eddb74e4ba99b1c
Ava M. Dunphy <ava.m.dunphy@gmail.com>
2025-04-29 14:09:17 -0400

    Jam

--- StreamlitAppFinal/app.py
+++ StreamlitAppFinal/app.py
@@ -20,4 +20,25 @@ st.markdown("""
 
 """)
 
-print(path)
+import streamlit as st
+
+st.title("🎶 Mood-Based Music Recommender 🎶")
+
+# Create tabs
+tab1, tab2, tab3 = st.tabs(["Take Mood Quiz", "Music Recommendations", "About This App"])
+
+# Tab 1 - Quiz
+with tab1:
+    st.header("📝 Mood Quiz")
+    st.write("This is where your quiz questions go!")
+
+# Tab 2 - Recommendations
+with tab2:
+    st.header("🎵 Music Recommendations")
+    st.write("After you find your mood, your music suggestions will show up here!")
+
+# Tab 3 - About
+with tab3:
+    st.header("ℹ️ About")
+    st.write("This app was built to help you discover new music based on your mood. Enjoy!")
+
