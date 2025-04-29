36c8516daf68d1a4644885d6920e0253cf58568b
Ava M. Dunphy <ava.m.dunphy@gmail.com>
2025-04-29 14:52:31 -0400

    Jam

--- StreamlitAppFinal/app.py
+++ StreamlitAppFinal/app.py
@@ -56,8 +56,8 @@ st.title("🎵 Spotify Mood Data Preview")
 st.write("Here are the first 10 rows of your dataset:")
 
 #Let's clean up the data set a little bit
-df_clean = df.drop(columns=['track_id', 'duration_ms' ])
-df_clean = df.drop(columns=["Unnamed: 0"])
+df_clean = df.drop(columns=['track_id', 'duration_ms', "Unnamed: 0"])
+
 df.columns = df.columns.str.replace('_', ' ')
 
 st.dataframe(df_clean.head(10))
